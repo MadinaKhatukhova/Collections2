33697235c5f8275e4187da38ecc107f37db9f6c3
Madina <madina-khat@yandex.ru>
2023-02-26 23:11:55 +0300

    Collections 3

--- src/Main.java
+++ src/Main.java
@@ -43,7 +43,39 @@ public class Main {
         List<Integer> integerList = new ArrayList<>();
         getEvenNumbers(integerSet, integerList);
         System.out.println("Оставшиеся четные числа из рандомных: " + integerList);
+
+        TelephoneDirectory telephoneDirectory = new TelephoneDirectory();
+        telephoneDirectory.addTelephoneOfPerson("Артур Иванов","89273456789");
+        telephoneDirectory.addTelephoneOfPerson("Петр Сидоров","89373357789");
+        telephoneDirectory.addTelephoneOfPerson("Николай Соболев","89973496789");
+        telephoneDirectory.addTelephoneOfPerson("Арсен Арзуманян","89873455889");
+        telephoneDirectory.addTelephoneOfPerson("Эдуард Суровый","89073866780");
+        telephoneDirectory.addTelephoneOfPerson("Максим Кожевников","82973456489");
+        telephoneDirectory.addTelephoneOfPerson("Юлия Сергеева","89053756889");
+        telephoneDirectory.addTelephoneOfPerson("Наталья Июльская","89593256780");
+        telephoneDirectory.addTelephoneOfPerson("Рената Майская","89373156689");
+        telephoneDirectory.addTelephoneOfPerson("Юлия Савичева","89373453783");
+        telephoneDirectory.addTelephoneOfPerson("Сергей Леонидович","89273456782");
+        telephoneDirectory.addTelephoneOfPerson("Иван Герасимов","89273456781");
+        telephoneDirectory.addTelephoneOfPerson("Альберт Эдуардович","89873456780");
+        telephoneDirectory.addTelephoneOfPerson("Олег Валентинович","89873456789");
+        telephoneDirectory.addTelephoneOfPerson("Игорь Сергеевич","89553456789");
+        telephoneDirectory.addTelephoneOfPerson("Жанна Виссарионовна","89373785789");
+        telephoneDirectory.addTelephoneOfPerson("Никита Покров","89073456745");
+        telephoneDirectory.addTelephoneOfPerson("Юрий Могучий","89173256759");
+        telephoneDirectory.addTelephoneOfPerson("Евгений Васильевич","89373556889");
+        telephoneDirectory.addTelephoneOfPerson("Татьяна Петровна","89573452789");
+
+        System.out.println(telephoneDirectory);
+
+        CollectionMapStringInteger collectionMapStringInteger= new CollectionMapStringInteger();
+        collectionMapStringInteger.addMap("Str1", 2);
+        collectionMapStringInteger.addMap("Str2", 1);
+        collectionMapStringInteger.addMap("Str1", 5);
+        System.out.println(collectionMapStringInteger);
     }
+
+
     public static void getEvenNumbers(Set<Integer> integerSet, List<Integer> integerList) {
         for (Integer integer : integerSet) {
             if (integer % 2 == 0) {
@@ -60,4 +92,6 @@ public class Main {
             i++;
         }
     }
+
+
 }
